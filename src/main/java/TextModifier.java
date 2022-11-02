ad551d714503f26db0f0ae3435ab7870d7e7304e
Kirsten Sutantyo <kirstensutantyo@gmail.com>
2022-11-02 16:02:30 -0400

    defined the MediaManager interface and defined the corresponding classes

---
+++ src/main/java/TextModifier.java
@@ -0,0 +1,28 @@
+public class TextModifier implements MediaManager{
+    private String link;
+    @Override
+    public void createObject() {
+
+    }
+
+    @Override
+    public void addMedia() {
+
+    }
+
+    @Override
+    public void modifyMedia() {
+
+    }
+
+    @Override
+    public void searchMedia() {
+
+    }
+
+    public void addLink(String givenLink){
+        this.link = givenLink;
+    }
+
+
+}
