23f9e18c177635d2e973bef57943ca94937babfd
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2022-11-02 16:06:13 -0400

    Defined classes and method headers under Drawable interface

---
+++ src/main/java/ShapeCreator.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ShapeCreator implements Drawable {
+
+    /** Takes in two positions from mouse input and creates a shape with the positions as endpoints and then
+     * call JavaFX shape thingy
+     */
+    @Override
+    public void useTwoPositions(double[] first, double[] second){
+
+    }
+}
+
