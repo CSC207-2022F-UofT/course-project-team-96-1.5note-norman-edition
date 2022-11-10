5686cd52313ccf9a53beb78dd8d917201f2f8e72
Dexter Gaon-Shatford <dexter@gaonshatford.ca>
2022-11-10 13:51:58 -0500

    add doc comment to GUIMediaFactory

--- src/main/java/gui/media/GUIMediaFactory.java
+++ src/main/java/gui/media/GUIMediaFactory.java
@@ -11,6 +11,14 @@ public class GUIMediaFactory {
 
     private GUIMediaFactory() {}
 
+    /**
+     * Instatiate the correct GUIMedia sub-class for the given Media object.
+     * <p>
+     * When we load a Media object from storage, we don't yet know specifically
+     * what type it is, but each type of Media has a different GUI representation,
+     * so we need to check the type of the Media and then create an instance of
+     * the correct sub-class of GUIMedia.
+     */
     public static GUIMedia getFor(Media media) throws Exception {
         if (media instanceof PenStroke) {
             return new GUIPenStroke((PenStroke) media);
