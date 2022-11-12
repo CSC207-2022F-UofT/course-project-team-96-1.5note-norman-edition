930c75c4e3d6e86121567803e0db84bf9809e1ec
Demise <liam.odero1@gmail.com>
2022-11-12 16:54:39 -0500

    Updated useTwoPositions to use Point2D objects instead of double[]s, and added a String 'shape' parameter

--- src/main/java/Drawable.java
+++ src/main/java/Drawable.java
@@ -1,6 +1,9 @@
+import javafx.geometry.Point2D;
+
+import java.awt.*;
 
 public interface Drawable {
     /** Takes in two positions from mouse input and does something with them
      */
-    public default void useTwoPositions(double[] first, double[] second){}
+    public default void useTwoPositions(Point2D first, Point2D second, String shape){}
 }
--- src/main/java/SelectionTool.java
+++ src/main/java/SelectionTool.java
@@ -1,9 +1,11 @@
+import javafx.geometry.Point2D;
+
 public class SelectionTool implements Drawable {
 
     /** Takes in two positions from mouse input and sends the area to the Zoomer
      */
     @Override
-    public void useTwoPositions(double[] first, double[] second){
+    public void useTwoPositions(Point2D first, Point2D second, String shape){
 
     }
 }
