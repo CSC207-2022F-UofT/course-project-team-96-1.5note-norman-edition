fd691cb686b9cadb90f453231d5e9c88f2a37db7
Demise <liam.odero1@gmail.com>
2022-11-12 16:45:14 -0500

    Added documentation to core classes and a subclass called PageFileMedia to differentiate media linked with external files. Also created MediaHyperlink class.

--- src/main/java/MediaAudio.java
+++ src/main/java/MediaAudio.java
@@ -1,29 +1,47 @@
-import javafx.scene.*;
+import javafx.scene.media.MediaPlayer;
+import java.util.ArrayList;
 
-public class MediaAudio extends PageMedia{
-    private PageMedia audio;
-    private MediaText[] timestamps; // TODO: Temp object type, feel free to change as needed.
+public class MediaAudio extends PageFileMedia{
+    //A subclass of PageFileMedia, defining Audio that exists on the page
+    //Instance Attributes:
+    //  audio: The GUI representation of the audio in question
+    //  timestamps: A set of timestamps that can be clicked associated with this audio
+    //  defaultVolume: default volume of the audio as defined by the audio files
+    private MediaPlayer audio;
+    private ArrayList<MediaHyperlink> timestamps;
+    private double defaultVolume;
 
-    public MediaAudio(double[] position, double[] dimensions, double angle, int zIndex, String name, String tag,
-                      PageMedia audio, byte[] rawData, MediaText[] timestamps) {
-        super(position, dimensions, angle, zIndex, name, tag, rawData);
+
+
+    public MediaAudio(double[] position, double[] dimensions, double angle, int zIndex, long id, String tag,
+                      MediaPlayer audio, byte[] rawData, ArrayList<MediaHyperlink>  timestamps) {
+        super(position, dimensions, angle, zIndex, id, tag, rawData);
         this.audio = audio;
         this.timestamps = timestamps;
+        this.defaultVolume = this.audio.getVolume();
     }
 
-    public PageMedia getAudio() {
+    public MediaPlayer getAudio() {
         return audio;
     }
 
-    public MediaText[] getTimestamps() {
+    public ArrayList<MediaHyperlink>  getTimestamps() {
         return timestamps;
     }
 
-    public void setAudio(PageMedia audio) {
+    public double getDefaultVolume() {
+        return defaultVolume;
+    }
+
+    public void setAudio(MediaPlayer audio) {
         this.audio = audio;
     }
 
-    public void setTimestamps(MediaText[] timestamps) {
+    public void setTimestamps(ArrayList<MediaHyperlink>  timestamps) {
         this.timestamps = timestamps;
+    }
+
+    public void setDefaultVolume(double defaultVolume) {
+        this.defaultVolume = defaultVolume;
     }
 }
