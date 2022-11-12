4e1f75693cce7146204d69b440a2a05adede24ac
Demise <liam.odero1@gmail.com>
2022-11-12 15:01:08 -0500

    Added capability to add timestamp hyperlinks that play requested parts of audio

--- src/main/java/AudioModifier.java
+++ src/main/java/AudioModifier.java
@@ -1,14 +1,16 @@
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+import java.util.ArrayList;
+
 
 public class AudioModifier implements MediaManager {
-
-    private String TimeStamp;
-
-    @Override
-    public void createObject() {
-
-    }
+    // Class defines methods to both create new MediaAudio objects and modify existing ones
+    //Instance Attributes:
+    //  timeStamp: A point in an audio file that an audio hyperlink will be made to
+    //  audio: a MediaAudio object to be modified
+    private Duration timeStamp;
+    private MediaAudio audio;
 
     @Override
     public void addMedia() {
@@ -24,16 +26,26 @@ public class AudioModifier implements MediaManager {
         //Creating related MediaAudio object
         Media audioUI = new Media("temp/");
         MediaPlayer mediaPlayer = new MediaPlayer(audioUI);
-
-        MediaAudio audio = new MediaAudio(new double[]{0, 0}, new double[]{0, 0}, 0, 0, "", "",
-                mediaPlayer, rawData, new MediaText[]{});
+        MediaAudio audio = new MediaAudio(new double[]{0, 0}, new double[]{0, 0}, 0, 0, 0, "",
+                mediaPlayer, rawData, new ArrayList<MediaHyperlink>());
 
         Page page = communicator.getPage();
         page.getElements().add(audio);
     }
 
+    //PRECONDITION: timeStamp is a valid point in audio
     @Override
     public void modifyMedia() {
+        //Calling TextModifier to create the neccessary Hyperlink
+        TextModifier textCreator = new TextModifier();
+        MediaHyperlink hyperlink = textCreator.createAudioHyperlink(this.timeStamp);
+
+        //Setting the hyperlink to play associated audio at requested time point
+        hyperlink.getText().setOnAction(e ->    {
+            this.audio.getAudio().seek(this.timeStamp);
+            this.audio.getAudio().play();
+        });
+        this.audio.getTimestamps().add(hyperlink);
 
     }
 
@@ -42,9 +54,14 @@ public class AudioModifier implements MediaManager {
 
     }
 
-    public void addTimeStamp(String givenTimeStamp){
-
-        this.TimeStamp = givenTimeStamp;
+    public void addTimeStamp(Duration givenTimeStamp){
+        this.timeStamp = givenTimeStamp;
     }
+
+    public void addMedia(MediaAudio audio)  {
+        this.audio = audio;
+    }
+
+    public MediaAudio getMedia()    {return this.audio; }
 
 }
--- src/main/java/TextModifier.java
+++ src/main/java/TextModifier.java
@@ -1,5 +1,9 @@
+import javafx.scene.control.Hyperlink;
+import javafx.util.Duration;
+
 public class TextModifier implements MediaManager{
     private String link;
+
     @Override
     public void addMedia() {
 
@@ -13,6 +17,12 @@ public class TextModifier implements MediaManager{
     @Override
     public void searchMedia() {
 
+    }
+
+    public MediaHyperlink createAudioHyperlink(Duration time)    { //TODO: delete if implemented hyperlink method makes this obsolete
+        Hyperlink timestamp = new Hyperlink(time.toString());
+        return new MediaHyperlink(new double[]{0, 0}, new double[]{0, 0}, 0, 0,
+                0, "", timestamp);
     }
 
     public void addLink(String givenLink){
