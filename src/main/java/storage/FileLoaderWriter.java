3a460f73833b93ddbe1462859e8243fba25de204
LiamOdero <liam.odero1@gmail.com>
2022-11-15 16:58:26 -0500

    implemented a preliminary way of reading and writing files

--- src/main/java/storage/FileLoaderWriter.java
+++ src/main/java/storage/FileLoaderWriter.java
@@ -1,6 +1,12 @@
 package storage;
-
+import javafx.stage.FileChooser;
 import storage.Storage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class FileLoaderWriter implements Storage {
     // TODO: Implement a file cache, which stores recently opened files to prevent consecutive reopening
@@ -8,11 +14,29 @@ public class FileLoaderWriter implements Storage {
 
     private byte[][] fileCache = new byte[cacheSize][]; // Internal file cache
 
-    public byte[] readFile(String path){
-        return new byte[1]; // TODO: Return a proper array of bytes
+    public byte[] readFile(String[] extensions){
+        FileChooser fileManager = new FileChooser();
+        fileManager.setTitle("Choose a File:");
+        for (String extension : extensions) {
+            fileManager.getExtensionFilters().add(new FileChooser.ExtensionFilter(extension));
+        }
+
+        File chosenFile = fileManager.showOpenDialog(null);
+        try {
+            return Files.readAllBytes(chosenFile.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e); //TODO: temp solution
+        }
     }
 
     public void writeFile(String path, byte[] Data){
-
+        try {
+            File newFile = new File(path);
+            FileOutputStream writer = new FileOutputStream(newFile);
+            writer.write(Data);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
